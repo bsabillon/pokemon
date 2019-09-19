ba53036295f129a9f57fcf76a4f769ea9cb23ab0
bsabillon <38368514+bsabillon@users.noreply.github.com>
2019-09-19 17:20:37 -0600

    added cors

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const database = require('./config/database');
+var cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
+app.use(cors());
 app.get('/',(req,res)=> res.send('Success'));
 
 database
