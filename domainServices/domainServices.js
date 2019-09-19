61c9613f688b54447eae1c19792751e647a4c1c8
bsabillon <38368514+bsabillon@users.noreply.github.com>
2019-09-18 22:24:33 -0600

    completed validations and added Jest

--- domainServices/domainServices.js
+++ domainServices/domainServices.js
@@ -3,27 +3,85 @@ const router = express.Router();
 const db = require('../config/database');
 const Pokemon = require('../models/Pokemon');
 const bodyParser = require('body-parser');
-var r = /^[A-Za-z]+$/ ;
+var containsOnlyStrings = /^[A-Za-z_ ]+$/; 
+var containsOnlyNumbers = /^[0-9]/;
 
 function isNameValid (name){
-   if(r.test(name)) {
-    return true;
-
-   }
-    else{
-         return false;
-        }
-
+   return (containsOnlyStrings.test(name))
 } 
 
+function areAbilitiesValid (abilities){
+    return (containsOnlyStrings.test(abilities))
+ } 
+
+ function areFormsValid (forms){
+    return (containsOnlyStrings.test(forms))
+ } 
+
+ function areHeldItemsValid (held_items){
+    return (containsOnlyStrings.test(held_items))
+ } 
+
+ function isLocationAreaValid (location_area_encounters){
+    return (containsOnlyStrings.test(location_area_encounters))
+ } 
+
+ function areMovesValid (moves){
+    return (containsOnlyStrings.test(moves))
+ } 
+
+ function areSpeciesValid (species){
+    return (containsOnlyStrings.test(species))
+ } 
+
+ function areTypesValid (types){
+    return (containsOnlyStrings.test(types))
+ } 
+
+ function isGameIndexValid (game_indices){
+    return (containsOnlyNumbers.test(game_indices))
+ } 
+
+ function isBaseExperienceValid (base_experience){
+    return (containsOnlyNumbers.test(base_experience))
+ }
+
+ function isHeightValid (height){
+    return (containsOnlyNumbers.test(height))
+ } 
+
+ function isOrdertValid (order){
+    return (containsOnlyNumbers.test(order))
+ } 
+
+ function areStatsValid (stats){
+    return (containsOnlyNumbers.test(stats))
+ } 
+
+ function isWeightValid (weight){
+    return (containsOnlyNumbers.test(weight))
+ } 
 
 var domainMethods = {
 
     isPokemonValid : (pokemon) => {
         return new Promise((resolve, reject) => {
-            console.log("POKETEST");
-            console.log(pokemon);
-           if(r.test(pokemon.name)) 
+            // console.log("POKETEST");
+            // console.log(pokemon);
+           if(isNameValid(pokemon.name)&&
+              areAbilitiesValid(pokemon.abilities)&&
+              areFormsValid(pokemon.forms)&&
+              areHeldItemsValid(pokemon.held_items)&&
+              isLocationAreaValid(pokemon.location_area_encounters)&&
+              areMovesValid(pokemon.moves)&&
+              areSpeciesValid(pokemon.species)&&
+              areTypesValid(pokemon.types)&&
+              isBaseExperienceValid(pokemon.base_experience)&&
+              isGameIndexValid(pokemon.game_indices)&&
+              isHeightValid(pokemon.height)&&
+              isOrdertValid(pokemon.order)&&
+              areStatsValid(pokemon.stats)&&
+              isWeightValid(pokemon.weight)  ) 
             {
                 resolve("Valid");
             }else{
@@ -33,24 +91,6 @@ var domainMethods = {
         })
     },
   
-
-    // isUserEmailValid: (userEmail )=>{
-    //     if ( userEmail.includes('@')){
-    //         return true;
-    //     }
-    //     else{
-    //         return false;
-    //     }
-    // },
-
-    // isUserPasswordValid: (userPassword )=>{
-    //     if ( userPassword.match(/^(?=.*\d)(?=(.*\W){2})(?=.*[a-zA-Z])(?!.*\s).{1,15}$/)){
-    //         return true;
-    //     }
-    //     else{
-    //         return false;
-    //     }
-    // }
 }
 
 module.exports= domainMethods;
